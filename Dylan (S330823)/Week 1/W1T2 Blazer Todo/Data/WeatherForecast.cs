b125a938383a5b30c3d788406ac87f4d46b5c9fe
DylanCDU <62042567+DylanCDU@users.noreply.github.com>
2021-08-18 14:42:52 +0930

    added todo

---
+++ Dylan (S330823)/Week 1/W1T2 Blazer Todo/Data/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace W1T2_Blazer_Todo.Data
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
