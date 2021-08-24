4efbaec340343f95aab6fcc045d44af7e6d9c865
Syed Rafay <rafay_syed1@yahoo.com>
2021-08-25 04:05:41 +0930

    Week 02 Using DataAnnotataion for Price and Display attribute for a column to make sure it looks elegant

--- Syed (S330705)/Week 02/MvcMovie/MvcMovie/Models/Movie.cs
+++ Syed (S330705)/Week 02/MvcMovie/MvcMovie/Models/Movie.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models
 {
@@ -13,9 +14,11 @@ namespace MvcMovie.Models
         public int Id { get; set; }
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
