627a31c40766f772754571a00105e2564ff90579
Syed Rafay <rafay_syed1@yahoo.com>
2021-08-25 05:06:48 +0930

    Week 02 Added a new field 'Rating' to the Movies

--- Syed (S330705)/Week 02/MvcMovie/MvcMovie/Models/Movie.cs
+++ Syed (S330705)/Week 02/MvcMovie/MvcMovie/Models/Movie.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -18,7 +13,9 @@ namespace MvcMovie.Models
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+        public string Rating { get; set; }
     }
 }
