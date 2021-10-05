9db78c3e390e13286b579070de423c556cf986b6
Syed Rafay <rafay_syed1@yahoo.com>
2021-10-05 21:54:31 +0930

    Week 07 Added TODO component and simple html to aurelia app

--- Syed (S330705)/Week 07/Aurelia-TODO/todo/src/app.ts
+++ Syed (S330705)/Week 07/Aurelia-TODO/todo/src/app.ts
@@ -1,3 +1,27 @@
-export class App {
-  public message = 'Hello World!';
+interface Todo {
+  description: string;
+  done: boolean;
 }
+
+export class App {
+  heading = "Todos";
+  todos: Todo[] = [];
+  todoDescription = '';
+
+  addTodo() {
+    if (this.todoDescription) {
+      this.todos.push({
+        description: this.todoDescription,
+        done: false
+      });
+      this.todoDescription = '';
+    }
+  }
+
+  removeTodo(todo) {
+    let index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+} 
